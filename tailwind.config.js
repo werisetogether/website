edd94dca8da85c5f52325f122cb4ecfeae865f33
Joshua Chew <joshuachew8118@gmail.com>
2021-06-29 22:06:52 +0800

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  mode: "jit",
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx,vue}",
-  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
