f63097a3715959d334782f3672e7be172aa377cf
Joshua Chew <joshuachew8118@gmail.com>
2021-07-21 10:59:43 +0800

    Remove purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   darkMode: false, // or 'media' or 'class'
-  purge: {
-    content: [
-      "./pages/**/*.{js,jsx,ts,tsx}",
-      "./Components/**/*.{js,jsx,ts,tsx}",
-    ],
-    safelist: ["html", "body"],
-  },
   theme: {
     fontFamily: {
       sans: ["Montserrat", "sans-serif"],
