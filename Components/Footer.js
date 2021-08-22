b4a81c8cac23b8fd5e8cd13a93918159c8761562
Joshua Chew <joshuachew8118@gmail.com>
2021-08-22 18:33:58 +0800

    Add membership

--- Components/Footer.js
+++ Components/Footer.js
@@ -6,6 +6,25 @@ const Footer = () => {
       <a href="#">
         <p className="underline text-center">Scroll to top</p>
       </a>
+      <div className="bg-yellow-100 border-2 border-black mt-4 rounded-xl flex flex-col justify-between  p-4">
+        <div className="">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            Join Membership
+          </h1>
+          <p className="text-sm sm:text-xl leading-7 sm:leading-9 font-normal mb-6">
+            We are a big family of passionate volunteers with a common goal in
+            mind. Join our membership to get the get the latets news and to show
+            your support!
+          </p>
+        </div>
+        <Link href="https://docs.google.com/forms/d/e/1FAIpQLSeTeX2zhmKt6GNRetwODA-6rQ5xWQNAoIW3t-BMh7Ih38huqA/viewform">
+          <a>
+            <button className="focus:outline-none mr-2 bg-white rounded-xl px-8 py-4 text-lg sm:text-xl font-semibold flex justify-center w-full h-full items-center hover:bg-gray-100 border-2 border-black">
+              Join now
+            </button>
+          </a>
+        </Link>
+      </div>
       <div className="py-8 border-t mt-4 border-gray-200 md:flex md:items-start md:justify-between">
         <div>
           <p className="text-base text-gray-600 mb-2 font-medium">
@@ -44,6 +63,6 @@ const Footer = () => {
       </div>
     </footer>
   );
-}
- 
+};
+
 export default Footer;
