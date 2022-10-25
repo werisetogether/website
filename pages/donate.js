908c2b4e623c8e1b213d9851d7116d3622f37ec5
werisetogether <86667799+werisetogether@users.noreply.github.com>
2022-10-25 18:40:52 +0530

    Google Analytics

--- pages/donate.js
+++ pages/donate.js
@@ -28,6 +28,15 @@ const Donate = ({ links }) => {
 	return (
 		<div>
 			<Head>
+		<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-36SZJWPBZW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-36SZJWPBZW');
+</script>
 				{/* Genral Tags */}
 				<title>We Rise Together Foundation — Donate</title>
 				<meta name="title" content="We Rise Together Foundation — Donate" />
